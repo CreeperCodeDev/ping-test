581a52117745f820b06f7ba9ed6ed2162ad030d0
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-06-11 13:45:20 +0530

    Break it right

--- ping.js
+++ ping.js
@@ -1,14 +1,20 @@
-function pingHost() {
+async function pingHost() {
   var protocol = document.getElementById("protocol").value;
   var host = document.getElementById("host").value;
+  if (!host) {
+    alert("host cannot be empty");
+    return;
+  }
   var interval = parseInt(document.getElementById("interval").value, 10);
+  if (isNaN(interval) || interval === 0) {
+    interval = 1;
+  }
   var url = protocol + "://" + host;
-  var errorOccurred = false; // Track error status
 
-  function makePingRequest() {
+  async function makePingRequest() {
     var startTime = Date.now();
 
-    fetch(url, {
+    await fetch(url, {
       method: "GET",
       mode: "no-cors"
     })
@@ -16,33 +22,33 @@ function pingHost() {
         var endTime = Date.now();
         var pingRate = endTime - startTime;
 
-        if (!errorOccurred) {
-          // Create a new row in the table
-          var table = document.getElementById("pingTable");
-          var row = table.insertRow(-1);
-          var hostCell = row.insertCell(0);
-          var pingRateCell = row.insertCell(1);
+        // Create a new row in the table
+        var table = document.getElementById("pingTable");
+        var row = table.insertRow(-1);
+        var hostCell = row.insertCell(0);
+        var pingRateCell = row.insertCell(1);
 
-          // Set the values in the table cells
-          hostCell.innerHTML = host;
-          pingRateCell.innerHTML = pingRate + "ms";
+        // Set the values in the table cells
+        hostCell.innerHTML = host;
+        pingRateCell.innerHTML = pingRate + "ms";
 
-          // Display the table
-          table.style.display = "table";
-        }
+        // Display the table
+        table.style.display = "table";
+        return true;
       })
       .catch(function(error) {
-        console.log("Error: " + error.message);
-        errorOccurred = true; // Set error status
+        throw error.message;
       });
   }
 
   // Start the ping requests in a loop
   for (var i = 0; i < interval; i++) {
-    makePingRequest();
-
-    if (errorOccurred) {
-      break; // Break the loop if an error occurred
+    try {
+      await makePingRequest();
+    } catch (error) {
+      console.log(error);
+      alert("Error: " + error);
+      break;
     }
   }
 }
